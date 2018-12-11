b99ab4adb676c0e8577f9be5bc22319d7a89e57c
CChandoke <44755739+CChandoke@users.noreply.github.com>
2018-12-10 20:30:06 -0600

    opterr variable in hand set to 0 to ignore extra args passed from dealer

--- hand.c
+++ hand.c
@@ -1,10 +1,11 @@
+
 #include <getopt.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#include <gsl/gsl_rng.h>	
+#include <gsl/gsl_rng.h>
 #include "commonFunc.c"
 
 int main(int argc, char * const argv[]) {
@@ -12,42 +13,48 @@ int main(int argc, char * const argv[]) {
 	//initialize/declare vars
 
 	int opt;
-	unsigned percentage = -1;
+	size_t percentage = -1;
 
 	//read cmdline args
 
-	while ((opt = getopt(argc, argv, "p:")) != -1) {
-		switch (opt) {
+	opterr = 0;		// set to 0 so getopt will ignore nonsense options 
+					// rather than output error messages by default
+	
+	while ((opt = getopt(argc, argv, "p:")) != -1) 
+		{
+		switch (opt) 
+			{
 			case 'p':
-				percentage = atoi(optarg);
+				percentage = atoi(optarg);		// convert percentage to integer so we can compare it
 				break;
-			default:
-				fprintf(stderr, "Usage: %s -p <percentage>\n", argv[0]);
-				exit(EXIT_FAILURE);
+			}
 		}
-	}
 
-	if (percentage == -1)
+	if (percentage == -1)	// error
 		printError("You need to specify a percentage!");
 
 	//main functionality
 
 	gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
 	struct timespec tp;
-	clock_gettime(CLOCK_REALTIME, &tp);
-	gsl_rng_set(rng, tp.tv_nsec);
+	clock_gettime(CLOCK_REALTIME, &tp);		// used as seed for random number generator
+	gsl_rng_set(rng, tp.tv_nsec);		// convert to nanoseconds (so seed is different for each 
+										// child process) and generate random number
 	if (rng == NULL)
 		printError("Failed to initialize GSL random number generator! Aborting.");
 
-	double randVal = gsl_rng_uniform (rng) * 100;
-	gsl_rng_free (rng);
+	double randVal = gsl_rng_uniform (rng) * 100;	// convert to percentage
+	gsl_rng_free (rng);		
 
-	if (randVal <= percentage) {
+	if (randVal <= percentage) 
+		{		
 		//printf("success!\n"); //testing
 		exit(TRIAL_SUCCESS);
-	} else {
+		} 
+	else 
+		{
 		//printf("failure!\n"); //testing
 		exit(TRIAL_FAILURE);
-	}
+		}
 }
 
